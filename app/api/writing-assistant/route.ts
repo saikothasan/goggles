cea84c25b37c53fabb1b1dfb382d76cda086d219
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2025-01-25 22:21:18 +0600

    Update route.ts

--- app/api/writing-assistant/route.ts
+++ app/api/writing-assistant/route.ts
@@ -1,16 +1,20 @@
 import { OpenAIStream, StreamingTextResponse } from "ai"
 import { Configuration, OpenAIApi } from "openai-edge"
 
+// Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 })
 const openai = new OpenAIApi(config)
 
+// IMPORTANT! Set the runtime to edge
 export const runtime = "edge"
 
 export async function POST(req: Request) {
+  // Extract the `prompt` from the body of the request
   const { prompt } = await req.json()
 
+  // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     stream: true,
@@ -26,7 +30,9 @@ export async function POST(req: Request) {
     ],
   })
 
+  // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response)
+  // Respond with the stream
   return new StreamingTextResponse(stream)
 }
 
